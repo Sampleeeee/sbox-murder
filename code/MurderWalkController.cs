fcd53396f8ecbe5e89c26d65fec9726df734e2be
Sampleeeee <behnersam@gmail.com>
2021-07-09 17:51:18 -0400

    Players can't move while they have black screen

--- code/MurderWalkController.cs
+++ code/MurderWalkController.cs
@@ -1,4 +1,5 @@
-﻿using Sandbox;
+﻿using Murder.Rounds;
+using Sandbox;
 
 namespace Murder
 {
@@ -8,6 +9,9 @@ namespace Murder
 		{
 			float ws = Duck.GetWishSpeed();
 			if ( ws >= 0 ) return ws;
+
+			if ( (Game.Current as MurderGame)?.Round is Preparing )
+				return 0f;
 
 			if ( Input.Down( InputButton.Run ) ) return Pawn.Tags.Has( "murderer" ) ? DefaultSpeed + 50f : DefaultSpeed;
 			return Input.Down( InputButton.Walk ) ? WalkSpeed : DefaultSpeed;
